931d7da3b8bb6901d146796c8a0d0fb7bf56a9a0
Klaus-poppe <vesparadkt@gmail.com>
2023-03-05 19:22:37 +0530

    added design. res shred and atk modifiers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { ChangeEvent, ReactNode, useState } from "react";
-import YaeMiko from "./assets/characters/yaeMiko.webp";
+import { ChangeEvent, ReactNode, useEffect, useState } from "react";
+import YaeMiko from "./assets/characters/yaeMiko_full.png";
 import hilichurl from "./assets/enemies/Enemy_Hilichurl.webp";
 import "./App.css";
+import anemoAuro from "./assets/misc/Element_Anemo.svg";
 
 function App() {
   const [character, setCharacter] = useState(defaultCharacterValues);
@@ -11,7 +12,7 @@ function App() {
     return (e: ChangeEvent<HTMLInputElement>) =>
       setCharacter((prev) => ({
         ...prev,
-        [stat]: parseInt(e.target.value),
+        [stat]: parseFloat(e.target.value),
       }));
   };
 
@@ -23,16 +24,27 @@ function App() {
       }));
   };
 
+  const results = calcDamage(character, enemy);
+
   return (
-    <div className="App">
+    <div className="app">
       <div className="main">
         <div className="char-info">
           <div className="char-preview-container">
-            <img className="char-preview" src={YaeMiko}></img>
+            <div className="char-name">Yae Miko</div>
+            <div className="char-aura-container">
+              <img className="char-aura" src={anemoAuro}></img>
+            </div>
+            <div className="char-image-container">
+              <img className="char-preview" src={YaeMiko}></img>
+            </div>
           </div>
+
           <div className="char-stats-container">
             <div className="char-stat-container">
-              <label htmlFor="charLevel">Level</label>
+              <label className="char-stat-input-label" htmlFor="charLevel">
+                Level
+              </label>
               <input
                 className="char-stat-input"
                 id="charLevel"
@@ -44,7 +56,22 @@ function App() {
               />
             </div>
             <div className="char-stat-container">
-              <label htmlFor="atk">Atk</label>
+              <label className="char-stat-input-label" htmlFor="baseAtk">
+                Base atk
+              </label>
+              <input
+                className="char-stat-input"
+                id="baseAtk"
+                type="number"
+                min="0"
+                value={character.baseAtk}
+                onChange={updateCharacterStats("baseAtk")}
+              />
+            </div>
+            <div className="char-stat-container">
+              <label className="char-stat-input-label" htmlFor="atk">
+                Atk
+              </label>
               <input
                 className="char-stat-input"
                 id="atk"
@@ -55,7 +82,9 @@ function App() {
               />
             </div>
             <div className="char-stat-container">
-              <label htmlFor="em">Em</label>
+              <label className="char-stat-input-label" htmlFor="em">
+                Em
+              </label>
               <input
                 className="char-stat-input"
                 id="em"
@@ -66,7 +95,9 @@ function App() {
               />
             </div>
             <div className="char-stat-container">
-              <label htmlFor="cRate">Crit Rate</label>
+              <label className="char-stat-input-label" htmlFor="cRate">
+                Crit Rate
+              </label>
               <input
                 className="char-stat-input"
                 id="cRate"
@@ -77,7 +108,9 @@ function App() {
               />
             </div>
             <div className="char-stat-container">
-              <label htmlFor="cDamage">Crit Damage</label>
+              <label className="char-stat-input-label" htmlFor="cDamage">
+                Crit Damage
+              </label>
               <input
                 className="char-stat-input"
                 id="cDamage"
@@ -88,7 +121,9 @@ function App() {
               />
             </div>
             <div className="char-stat-container">
-              <label htmlFor="dmgBonus">Damage Bonus</label>
+              <label className="char-stat-input-label" htmlFor="dmgBonus">
+                Damage Bonus
+              </label>
               <input
                 className="char-stat-input"
                 id="dmgBonus"
@@ -99,7 +134,12 @@ function App() {
               />
             </div>
             <div className="char-stat-container">
-              <label htmlFor="skillMultiplier">Skill Multiplier</label>
+              <label
+                className="char-stat-input-label"
+                htmlFor="skillMultiplier"
+              >
+                Skill Multiplier
+              </label>
               <input
                 className="char-stat-input"
                 id="skillMultiplier"
@@ -109,28 +149,91 @@ function App() {
                 onChange={updateCharacterStats("skillMultiplier")}
               />
             </div>
-            <pre>{JSON.stringify(character, null, 2)}</pre>
+            {/* <pre>{JSON.stringify(character, null, 2)}</pre> */}
+          </div>
+
+          <div className="buffs-container">
+            <div className="buffs">
+              <div className="buff">
+                <label htmlFor="atkPercent" className="char-stat-input-label">
+                  Atk percent
+                </label>
+                <input
+                  className="char-stat-input"
+                  id="atkPercent"
+                  type="number"
+                  min="0"
+                  max="100"
+                  value={character.atkPercent}
+                  onChange={updateCharacterStats("atkPercent")}
+                />
+              </div>
+              <div className="buff">
+                <label htmlFor="flatAtkBonus" className="char-stat-input-label">
+                  Flat Atk Bonus
+                </label>
+                <input
+                  className="char-stat-input"
+                  id="flatAtkBonus"
+                  type="number"
+                  min="0"
+                  max="100"
+                  value={character.flatAtkBonus}
+                  onChange={updateCharacterStats("flatAtkBonus")}
+                />
+              </div>
+              <div className="buff">
+                <label htmlFor="flatEmBonus" className="char-stat-input-label">
+                  EM bonus
+                </label>
+                <input
+                  className="char-stat-input"
+                  id="flatEmBonus"
+                  type="number"
+                  min="0"
+                  max="100"
+                  value={character.flatEmBonus}
+                  onChange={updateCharacterStats("flatEmBonus")}
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="dmg-results">
-          <div className="char-preview-container">
-            <img className="char-preview" src={hilichurl} />
+
+        <div className="enemy-info char-info ">
+          <div className="results-contianer">
+            <label htmlFor="bdmgRes" className="char-stat-input-label">
+              BASE DMG
+            </label>
+            <div id="bdmgRes" className="result">
+              {results.dmg}
+            </div>
+            <label htmlFor="cdmgRes" className="char-stat-input-label">
+              CRIT DMG
+            </label>
+            <div id="cdmgRes" className="result">
+              {results.critDmg}
+            </div>
           </div>
           <div className="char-stats-container">
             <div className="char-stat-container">
-              <label htmlFor="enemyLevel">Enemy Level</label>
+              <label className="char-stat-input-label" htmlFor="enemyLevel">
+                Level
+              </label>
               <input
                 className="char-stat-input"
                 id="enemyLevel"
                 type="number"
                 min="0"
-                max="90"
+                max="100"
                 value={enemy.level}
                 onChange={updateEnemyStats("level")}
               />
             </div>
             <div className="char-stat-container">
-              <label htmlFor="resistance">Resistance</label>
+              <label className="char-stat-input-label" htmlFor="resistance">
+                Resistance
+              </label>
               <input
                 className="char-stat-input"
                 id="resistance"
@@ -142,7 +245,9 @@ function App() {
               />
             </div>
             <div className="char-stat-container">
-              <label htmlFor="defReduction">Enemy Def Reduction</label>
+              <label className="char-stat-input-label" htmlFor="defReduction">
+                Def Reduction
+              </label>
               <input
                 className="char-stat-input"
                 id="defReduction"
@@ -153,7 +258,9 @@ function App() {
               />
             </div>
             <div className="char-stat-container">
-              <label htmlFor="defIgnore">Enemy Def Ignore</label>
+              <label className="char-stat-input-label" htmlFor="defIgnore">
+                Def Ignore
+              </label>
               <input
                 className="char-stat-input"
                 id="defIgnore"
@@ -163,57 +270,148 @@ function App() {
                 onChange={updateEnemyStats("defIgnore")}
               />
             </div>
+            <div className="char-stat-container">
+              <label className="char-stat-input-label" htmlFor="resShred">
+                Resitance shred
+              </label>
+              <input
+                className="char-stat-input"
+                id="resShred"
+                type="number"
+                min="0"
+                value={enemy.resShred}
+                onChange={updateEnemyStats("resShred")}
+              />
+            </div>
           </div>
 
-          <pre>{JSON.stringify(enemy, null, 2)}</pre>
+          <div>
+            <div className="char-aura-container">
+              <img className="char-aura" src={anemoAuro}></img>
+            </div>
+            <div className="enemy-preview-container">
+              <img className="enemy-preview" src={hilichurl} />
+            </div>
+          </div>
+
+          {/* <pre>{JSON.stringify(enemy, null, 2)}</pre> */}
         </div>
       </div>
-      <div className="results-contianer">{calcDamage()}</div>
+      {/* <div className="results-contianer">{calcDamage(character, enemy)}</div> */}
     </div>
   );
 }
 
-const defaultCharacterValues = {
+type character = {
+  level: number;
+  baseAtk: number;
+  atk: number;
+  em: number;
+  cRate: number;
+  cDamage: number;
+  dmgBonus: number;
+  skillMultiplier: number;
+  atkPercent: number;
+  flatAtkBonus: number;
+  flatEmBonus: number;
+  Aura: string;
+  FlatDmgBonus: number;
+};
+
+type enemy = {
+  level: number;
+  resistance: number;
+  defReduction: number;
+  defIgnore: number;
+  resShred: number;
+};
+
+const defaultCharacterValues: character = {
   level: 90,
+  baseAtk: 0,
   atk: 0,
   em: 0,
   cRate: 0,
   cDamage: 0,
   dmgBonus: 0,
   skillMultiplier: 0,
+  atkPercent: 0,
+  flatAtkBonus: 0,
+  flatEmBonus: 0,
+  Aura: "none",
+  FlatDmgBonus: 0,
 };
 
-const defaultEnemyValues = {
+const defaultEnemyValues: enemy = {
   level: 90,
   resistance: 10,
   defReduction: 0,
   defIgnore: 0,
+  resShred: 0,
 };
 
-const calcDamage = () => {
+const calcResMultiplier = (
+  resistance: number = 0,
+  resShred: number = 0
+): number => {
+  let res = (resistance - resShred) / 100;
+
+  if (res < 0) return 1 - res / 2;
+
+  if (res >= 75) return 1 / (4 * res + 1);
+
+  return 1 - res;
+};
+
+const calcDamage = (char: character, enemy: enemy) => {
   // const baseDmg = skill * atk/def/hp
 
-  const skill = 170 / 100;
-  const atk = 1464;
-  const em = 273;
-  const eDmgBonus = 61 / 100;
-  const cRate = 85;
-  const cDamage = 203 / 100;
+  // const skill = 170 / 100;
+  // const atk = 1464;
+  // const em = 273;
+  // const eDmgBonus = 61 / 100;
+  // const cRate = 85;
+  // const cDamage = 203 / 100;
+  // const sesshouSakuraBonus = (em * 0.15) / 100;
+  // const defReduction = 0;
+  // const defIgnore = 0;
+  // const charLevel = 90;
+  // const enemyLevel = 90;
+  // const enemyResistance = 10;
+
+  const charLevel = char.level || 0;
+  const skill = (char.skillMultiplier || 0) / 100;
+
+  // use Math.round to potentially increase accuracy
+  const atk =
+    (char.atk || 0) +
+    (char.baseAtk * char.atkPercent) / 100 +
+    char.flatAtkBonus;
+  const em = char.em || 0;
+  const eDmgBonus = (char.dmgBonus || 0) / 100;
+  const cRate = char.cRate || 0;
+  const cDamage = (char.cDamage || 0) / 100;
   const sesshouSakuraBonus = (em * 0.15) / 100;
-  const defReduction = 0;
-  const defIgnore = 0;
-  const charLevel = 90;
-  const enemyLevel = 90;
+
+  const defReduction = enemy.defReduction || 0;
+  const defIgnore = enemy.defIgnore || 0;
+  const enemyLevel = enemy.level || 0;
+  // const enemyResistance = calcResMultiplier(
+  //   enemy.resistance || 0,
+  //   enemy.resShred || 0
+  // );
 
   const baseDmg = skill * atk;
   const dmgBonus = eDmgBonus + sesshouSakuraBonus;
   const k = (1 - defReduction) * (1 - defIgnore);
 
-  const enemyResistance = 10;
-
   const defMultiplierEnemy =
     (charLevel + 100) / (k * (enemyLevel + 100) + (charLevel + 100));
-  const resMultiplierEnemy = 1 - enemyResistance / 100;
+
+  const resMultiplierEnemy = calcResMultiplier(
+    enemy.resistance || 0,
+    enemy.resShred || 0
+  );
 
   const dmg =
     baseDmg * (1 + dmgBonus) * (defMultiplierEnemy * resMultiplierEnemy);
@@ -221,108 +419,113 @@ const calcDamage = () => {
   const critDmg = dmg * (1 + cDamage);
 
   // const dmg =
-  //   (baseDmg * specialMultiplier + FlatDmgBonus) *
+  //   ((baseDmg * specialMultiplier) + FlatDmgBonus) *
   //   (1 + percentageDmgBonus - DmgReductionTarget) *
   //   (defMultiplierTarget * ResMultipliesTarget) *
   //   AmplifyingMultiplier;
 
   // const critDamage = Dmg * (1 + critDamage)
 
-  return `Yae Miko does ${dmg.toFixed(2)} damage against a lvl 90 hilichurl
-  and crit damage of ${critDmg.toFixed(2)}
-
-  baseDmg = ${baseDmg.toFixed(2)}
-  dmgBonus = ${dmgBonus.toFixed(2)}
-  defMultiplierEnemy = ${defMultiplierEnemy.toFixed(2)}
-  resMultiplierEnemy = ${resMultiplierEnemy.toFixed(2)}
-  
-  `;
-};
-
-const characters: characters = {
-  yae: {
-    lvl: 90,
-    bHp: 10372,
-    bAtk: 339,
-    bDef: 568,
-    em: 0,
-    er: 100,
-    cRate: 24.2,
-    cDamge: 50,
-    dmgBonus: 0,
-    atkPercent: 50,
-    atk: function () {
-      return this.bAtk * (1 + this.atkPercent / 100);
-    },
-  },
-};
-
-const weapons = {
-  widsith: {
-    lvl: 90,
-    refinement: 5,
-    mainStat: 510,
-    subStat: {
-      stat: "cDamage",
-      value: "55.1",
-    },
-    passive: {
-      status: "active",
-      stat: "em",
-      value: 480,
-    },
-  },
-};
-
-const enemies = {
-  hilichurl: {
-    lvl: 90,
-    hp: 24354,
-    atk: 3426,
-    def: 1000,
-    resistance: 10,
-  },
-};
-
-type characters = {
-  [charName: string]: character;
-};
-
-type character = {
-  bHp: number;
-  lvl: number;
-  bAtk: number;
-  bDef: number;
-  em: number;
-  er: number;
-  cRate: number;
-  cDamge: number;
-  atkPercent: number;
-  dmgBonus: number;
-  atk: Function;
-};
-
-type weapon = {
-  lvl: number;
-  refinement: number;
-  mainStat: number;
-  substat: {
-    stat: string;
-    value: number;
+  return {
+    dmg: Math.round(dmg),
+    critDmg: Math.round(critDmg),
+    // dmg: dmg.toFixed(2),
+    // critDmg: critDmg.toFixed(2),
   };
-  passive: {
-    status: string;
-    stat: string;
-    value: number;
-  };
+
+  // return `Yae Miko does ${dmg.toFixed(2)} damage against a lvl ${
+  //   enemy.level
+  // } hilichurl
+  // and crit damage of ${critDmg.toFixed(2)}
+
+  // baseDmg = ${baseDmg.toFixed(2)}
+  // dmgBonus = ${dmgBonus.toFixed(2)} `;
 };
 
-type enemy = {
-  lvl: number;
-  hp: number;
-  atk: number;
-  def: number;
-  resistance: number;
-};
+// const characters: characters = {
+//   yae: {
+//     lvl: 90,
+//     bHp: 10372,
+//     bAtk: 339,
+//     bDef: 568,
+//     em: 0,
+//     er: 100,
+//     cRate: 24.2,
+//     cDamge: 50,
+//     dmgBonus: 0,
+//     atkPercent: 50,
+//     atk: function () {
+//       return this.bAtk * (1 + this.atkPercent / 100);
+//     },
+//   },
+// };
+
+// const weapons = {
+//   widsith: {
+//     lvl: 90,
+//     refinement: 5,
+//     mainStat: 510,
+//     subStat: {
+//       stat: "cDamage",
+//       value: "55.1",
+//     },
+//     passive: {
+//       status: "active",
+//       stat: "em",
+//       value: 480,
+//     },
+//   },
+// };
+
+// const enemies = {
+//   hilichurl: {
+//     lvl: 90,
+//     hp: 24354,
+//     atk: 3426,
+//     def: 1000,
+//     resistance: 10,
+//   },
+// };
+
+// type characters = {
+//   [charName: string]: character;
+// };
+
+// type character = {
+//   bHp: number;
+//   lvl: number;
+//   bAtk: number;
+//   bDef: number;
+//   em: number;
+//   er: number;
+//   cRate: number;
+//   cDamge: number;
+//   atkPercent: number;
+//   dmgBonus: number;
+//   atk: Function;
+// };
+
+// type weapon = {
+//   lvl: number;
+//   refinement: number;
+//   mainStat: number;
+//   substat: {
+//     stat: string;
+//     value: number;
+//   };
+//   passive: {
+//     status: string;
+//     stat: string;
+//     value: number;
+//   };
+// };
+
+// type enemy = {
+//   lvl: number;
+//   hp: number;
+//   atk: number;
+//   def: number;
+//   resistance: number;
+// };
 
 export default App;
